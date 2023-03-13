cd302703846bda22cde65667121a9dfb581ad2f0
Андрей Захаров <andryushka.zakharov.04@mail.ru>
2023-03-13 20:08:22 +0700

    Adding an authorization method

---
+++ For-A-Donation/Models/ViewModels/User/UserViewModelAuthorization.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace For_A_Donation.Models.ViewModels.User;
+
+public class UserViewModelAuthorization
+{
+    [Phone]
+    [Required]
+    public string? PhoneNumber { get; set; }
+
+    [Required]
+    public string? Password { get; set; }
+}
