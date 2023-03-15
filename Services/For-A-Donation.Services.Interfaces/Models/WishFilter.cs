7fbfad9b0a2448a4b99e0aec9f72815e04d2feb4
Андрей Захаров <andryushka.zakharov.04@mail.ru>
2023-03-15 19:41:10 +0700

    Debugging

---
+++ Services/For-A-Donation.Services.Interfaces/Models/WishFilter.cs
@@ -0,0 +1,12 @@
+﻿using For_A_Donation.Domain.Core.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace For_A_Donation.Services.Interfaces.Models;
+
+public class WishFilter
+{
+    public Guid? UserId { get; set; }
+
+    [Range(0, 4)]
+    public CategoryOfReward? Category { get; set; }
+}
